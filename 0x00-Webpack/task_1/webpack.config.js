a6f4c9364493353b2b1009f40b33e33e08b6d3cf
AbduHany <abdu.hany@gmail.com>
2024-08-19 19:01:34 +0300

    Modify webpack config

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: {
         main: path.resolve(__dirname, './js/dashboard_main.js'),
     },
-    mode: 'production',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
