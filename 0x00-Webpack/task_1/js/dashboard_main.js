34447158b866acc30db33e6037cb17e409a2139c
AbduHany <abdu.hany@gmail.com>
2024-08-19 19:15:44 +0300

    Modify dashboard_main

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,10 +2,10 @@ import $ from "jquery";
 import _ from 'lodash';
 
 
-let clickCount = 0
+let clickCount = 0;
 
 function updateCounter() {
-    clickCount += 1
+    clickCount += 1;
     $('#count').text(`${clickCount} clicks on the button`);
 }
 
@@ -15,5 +15,5 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-const debouncedUpdateCounter = _.debounce(updateCounter, 300, { leading: true, trailing: false });
+const debouncedUpdateCounter = _.debounce(updateCounter, 500, { leading: true, trailing: false });
 $('button').on('click', debouncedUpdateCounter);
