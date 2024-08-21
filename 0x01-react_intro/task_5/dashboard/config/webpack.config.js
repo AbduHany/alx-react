55ffec9ccfe758f9bfd6e8a18af53b085de0479e
AbduHany <abdu.hany@gmail.com>
2024-08-21 13:48:21 +0300

    Add task_5 files

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: {
+        main: path.resolve(__dirname, '../src/index.js'),
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    devServer: {
+        static: path.join(__dirname, '../dist'),
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(js|jsx)$/, // Match .js and .jsx files
+                exclude: /node_modules/, // Exclude node_modules directory
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'], // Use Babel presets
+                    },
+                },
+            }
+        ]
+    },
+}
---
+++ 0x01-react_intro/task_5/dashboard/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils.js';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <img width={200} height={200} src={logo} alt="Holberton School logo"></img>
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label>Email:</label>
+        <input type="email" id="email" />
+        <label>Password:</label>
+        <input type="password" id="password"></input>
+        <button className='ok'>OK</button>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
