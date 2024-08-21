2f0349423fef95e2082f8f5c228b1595725b4ba1
AbduHany <abdu.hany@gmail.com>
2024-08-21 13:03:59 +0300

    Add task_4 files

---
+++ 0x01-react_intro/task_4/dashboard/src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App'
+
+describe('<App />', () => {
+    it('all of App renders', () => {
+        shallow(<App />);
+    });
+
+    it('renders a div with class App-header', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('div .App-header').exists()).toBe(true);
+    });
+
+    it('renders a div with class App-body', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('div .App-body').exists()).toBe(true);
+    });
+
+    it('renders a div with class App-footer', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('div .App-footer').exists()).toBe(true);
+    });
+});
