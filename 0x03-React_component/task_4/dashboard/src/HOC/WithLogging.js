e58fedbaa69e135cc7cbf81ffcbdc43efe6a16bc
AbduHany <abdu.hany@gmail.com>
2024-08-29 20:26:44 +0300

    Add task 7 files

---
+++ 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.js
@@ -0,0 +1,21 @@
+export const WithLogging = (WrappedComponent, id) => {
+    const wrapper = class extends React.Component {
+        constructor(props) {
+            super(props);
+        }
+
+        componentDidMount() {
+            console.log(`Component ${WrappedComponent.name || 'Component'} is mounted`);
+        }
+
+        componentWillUnmount() {
+            console.log(`Component ${WrappedComponent.name || 'Component'} is going to unmount`);
+        }
+
+        render() {
+            return <WrappedComponent {...this.props} />;
+        }
+    };
+    wrapper.displayName = `WithLogging(${WrappedComponent.name || 'Component'})`;
+    return wrapper;
+};
