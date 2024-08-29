d9c63d7966f37fc47e18b698a7b36777dc4a9fe2
AbduHany <abdu.hany@gmail.com>
2024-08-29 20:55:09 +0300

    Implement the shouldComponentUpdate

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -6,42 +6,6 @@ import { getLatestNotification } from '../utils/utils';
 import PropTypes from 'prop-types';
 import NotificationItemShape from './NotificationItemShape';
 
-// const Notifications = ({ displayDrawer = false, listNotifications = [] }) => {
-//     return (
-//         <>
-//             <div className='menuItem'>
-//                 <p>Your notifications</p>
-//             </div>
-//             {displayDrawer &&
-//                 <div className='Notifications'>
-//                     <ul>
-//                         {listNotifications.length === 0 ?
-//                             <NotificationItem type='default' value="No new notification for now" /> :
-//                             <>
-//                                 <p>Here is the list of notifications</p>
-//                                 {listNotifications.map((notifiction) => {
-//                                     return <NotificationItem key={notifiction.id} type={notifiction.type} value={notifiction.value} html={notifiction.html} />
-//                                 })}
-//                             </>
-//                         }
-//                     </ul>
-//                     <button style={{
-//                         marginRight: '10px',
-//                         backgroundColor: 'white',
-//                         border: '1px solid lightgrey',
-//                         borderRadius: '5px',
-//                         padding: '3px',
-//                         position: 'absolute',
-//                         right: '30px',
-//                         top: '15px',
-//                     }} aria-label='close' onClick={() => { console.log("Close button has been clicked") }}>
-//                         <img width="10px" src={closeIcon} alt="close button"></img>
-//                     </button>
-//                 </div >
-//             }
-//         </>
-//     );
-// };
 
 class Notifications extends React.Component {
     constructor(props) {
@@ -51,6 +15,10 @@ class Notifications extends React.Component {
 
     markAsRead(id) {
         console.log(`Notification ${id} has been marked as read`);
+    }
+
+    shouldComponentUpdate(newProps) {
+        return newProps.listNotifications.length > this.props.listNotifications.length;
     }
 
     render() {
