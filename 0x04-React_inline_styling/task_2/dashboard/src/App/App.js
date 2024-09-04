7030b70aa1c97447cc9cc282e42e65f0341a97cb
AbduHany <abdu.hany@gmail.com>
2024-09-04 04:21:50 +0300

    Fix Task2 conditions

--- 0x04-React_inline_styling/task_2/dashboard/src/App/App.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/App/App.js
@@ -1,99 +1,72 @@
-import React from 'react';
-import Header from '../Header/Header';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-import Notifications from '../Notifications/Notifications';
-import CourseList from '../CourseList/CourseList';
-import PropTypes from 'prop-types';
-import { getLatestNotification } from '../utils/utils';
-import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
-import BodySection from '../BodySection/BodySection';
-import { StyleSheet, css } from 'aphrodite';
+import React, { Component } from "react";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import BodySection from "../BodySection/BodySection";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Footer from "../Footer/Footer";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
+import { StyleSheet, css } from "aphrodite";
 
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
 
-const styles = StyleSheet.create({
-  "App": {
-    margin: 0,
-    padding: 0,
-    fontFamily: "sans-serif",
-    boxSizing: "border-box",
-    maxHeight: "100vh",
-  },
-  "AppBody": {
-    padding: "50px",
-    fontSize: "1.5rem",
-    height: "59vh",
-  },
-  "AppHeader": {
-    display: "flex",
-    alignItems: "center",
-    color: "#e1003c",
-    borderBottom: "5px solid #e1003c",
-    paddingLeft: "20px",
-  },
-  'AppFooter': {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderTop: "5px solid #e1003c",
-    fontStyle: "italic",
-    fontSize: "large",
-  }
-});
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+];
 
-class App extends React.Component {
-
-  handleKeydown = (event) => {
-    if (event.ctrlKey && event.key === 'h') {
-      event.preventDefault();
-      alert('Logging you out');
-      this.props.logOut();
-    }
-  };
-
+class App extends Component {
   constructor(props) {
     super(props);
-    this.handleKeydown = this.handleKeydown.bind(this);
+    this.handleKeyCombination = this.handleKeyCombination.bind(this);
+  }
+
+  handleKeyCombination(e) {
+    if (e.key === "h" && e.ctrlKey) {
+      alert("Logging you out");
+      this.props.logOut();
+    }
   }
 
   componentDidMount() {
-    document.addEventListener('keydown', this.handleKeydown);
+    document.addEventListener("keydown", this.handleKeyCombination);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeydown);
+    document.removeEventListener("keydown", this.handleKeyCombination);
   }
 
   render() {
-    const listCourses = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 }
-    ];
-
-    const listNotifications = [
-      { id: 1, type: "default", value: "New course available" },
-      { id: 2, type: "urgent", value: "New resume available" },
-      { id: 3, type: "urgent", html: { __html: getLatestNotification() } }
-    ];
-
     return (
       <>
-        <div className={css(styles.App)}>
-          <Notifications listNotifications={listNotifications} />
-          <div className={css(styles.AppHeader)}>
-            <Header />
-          </div>
-          <div className={css(styles.AppBody)}>
-            {this.props.isLoggedIn ?
-              <BodySectionWithMarginBottom title="Course list"><CourseList listCourses={listCourses} /></BodySectionWithMarginBottom> :
-              <BodySectionWithMarginBottom title="Log in to continue"><Login /></BodySectionWithMarginBottom>
-            }
-            <BodySection title="News from the School"><p>Abdelrahman Hany is the BEST developer ever! He'll conquer the world!</p></BodySection>
-          </div>
-          <div className={css(styles.AppFooter)}>
-            <Footer />
-          </div>
+        <Notifications listNotifications={listNotifications} />
+        <div className={css(styles.header)}>
+          <Header />
+        </div>
+        <div className={css(styles.body)}>
+          {!this.props.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={listCourses} />
+            </BodySectionWithMarginBottom>
+          )}
+        </div>
+        <BodySection title="News from the School">
+          <p>Some Random Text</p>
+        </BodySection>
+
+        <div className={css(styles.footer)}>
+          <Footer />
         </div>
       </>
     );
@@ -102,13 +75,37 @@ class App extends React.Component {
 
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => { }
+  logOut: () => { },
 };
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func
+  logOut: PropTypes.func,
 };
 
+const cssVars = {
+  mainColor: "#e01d3f",
+};
+
+const styles = StyleSheet.create({
+  header: {
+    borderBottom: `3px solid ${cssVars.mainColor}`,
+  },
+
+  body: {
+    display: "flex",
+    justifyContent: "center",
+  },
+
+  footer: {
+    borderTop: `3px solid ${cssVars.mainColor}`,
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    position: "fixed",
+    bottom: 0,
+    fontStyle: "italic",
+  },
+});
 
 export default App;
