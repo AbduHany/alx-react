84b553b94d2d29eaa367ca99c918271d12e992ce
AbduHany <abdu.hany@gmail.com>
2024-09-12 09:15:20 +0000

    Add edge case 2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,7 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
+  if (object === undefined || array === undefined) return undefined;
   const immutableObject = fromJS(object);
   let value = immutableObject;
   for (const path of array) {
