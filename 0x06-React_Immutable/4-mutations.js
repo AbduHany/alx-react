7ad31352cf460c0fd31c5b8cfd7f782439887e05
AbduHany <abdu.hany@gmail.com>
2024-09-12 09:48:12 +0000

    Add 4-mutations.js

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,14 @@
+import { fromJS } from 'immutable';
+
+const map = fromJS({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+
+export { map, map2 };
