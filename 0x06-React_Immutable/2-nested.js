af3af655497321bb2d3edb89aa500360c8219e83
AbduHany <abdu.hany@gmail.com>
2024-09-12 09:19:32 +0000

    use getIn in 2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,12 +1,6 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  if (object === undefined || array === undefined) return undefined;
   const immutableObject = fromJS(object);
-  let value = immutableObject;
-  for (const path of array) {
-    if (value === undefined) return undefined;
-    value = value.get(path);
-  }
-  return (value);
+  return (immutableObject.getIn(array));
 }
