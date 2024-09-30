5f4a4eb518707064a68c0f7df51ec99bbf4849e3
AbduHany <abdu.hany@gmail.com>
2024-09-30 08:56:51 +0300

    Change Footer to only use the user prop

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { getFullYear, getFooterCopy } from '../utils/utils';
-import AppContext from '../App/AppContext';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Footer = ({ isLoggedIn = false, user = {} }) => {
+const Footer = ({ user }) => {
 
   return (
     <>
       <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      {isLoggedIn && (
+      {user && (
         <a href="#">Contact us</a>
       )}
     </>
   );
 };
 
+Footer.propTypes = {
+  user: PropTypes.object
+};
+
+Footer.defaultProps = {
+  user: null
+}
+
 export const mapStateToProps = (state) => {
   return {
     user: state.get('user'),
-    isLoggedIn: state.get('isUserLoggedIn'),
   };
 };
 
